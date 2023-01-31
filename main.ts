fb00bbbacaca66e8b88b009f8f7e4a199a9b4e55
Sainz31 <122534761+Sainz31@users.noreply.github.com>
2023-01-31 13:48:49 +0100

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,75 @@
-basic.forever(function () {
-	
+radio.onReceivedNumber(function (receivedNumber) {
+    while (enviado == 0) {
+        basic.pause(100)
+    }
+    basic.pause(100)
+    basic.clearScreen()
+    if (receivedNumber == 1) {
+        basic.showLeds(`
+            . # # # .
+            # # # # #
+            # # # # #
+            # # # # #
+            . # # # .
+            `)
+    }
+    if (receivedNumber == 2) {
+        basic.showLeds(`
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+    }
+    if (receivedNumber == 3) {
+        basic.showLeds(`
+            # . . . #
+            . # . # .
+            . . # . .
+            # # . # #
+            # # . # #
+            `)
+    }
 })
+input.onButtonPressed(Button.A, function () {
+    basic.showLeds(`
+        . # # # .
+        # # # # #
+        # # # # #
+        # # # # #
+        . # # # .
+        `)
+    radio.sendNumber(1)
+    serial.writeString("piedra")
+    basic.pause(100)
+    enviado += 1
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showLeds(`
+        # . . . #
+        . # . # .
+        . . # . .
+        # # . # #
+        # # . # #
+        `)
+    radio.sendNumber(3)
+    serial.writeString("tijera")
+    basic.pause(100)
+    enviado += 1
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showLeds(`
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        `)
+    radio.sendNumber(2)
+    serial.writeString("papel")
+    basic.pause(100)
+    enviado += 1
+})
+let enviado = 0
+radio.setGroup(138)
